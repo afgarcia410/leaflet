4dad3635f70818ab0c163ce2dc41337d10edffef
afgarcia410 <115699382+afgarcia410@users.noreply.github.com>
2023-01-25 12:46:47 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+var map = L.map('map').setView([40.4637, 3.7492], 4);
+
+var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+L.geoJson(provincia).addTo(map);
+function getColor(d) {
+    return d > "andaluc\u00eda" ? '#800026' :
+           d > 500  ? '#BD0026' :
+           d > 200  ? '#E31A1C' :
+           d > 100  ? '#FC4E2A' :
+           d > 50   ? '#FD8D3C' :
+           d > 20   ? '#FEB24C' :
+           d > "Catalu\u00f1a"   ? '#FED976' :
+                      '#FFEDA0';
+}
+function style(feature) {
+    return {
+        fillColor: getColor(features.properties.ccaa),
+        weight: 2,
+        opacity: 1,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.7
+    };
+}
+
+L.geoJson(provincia, {style: style}).addTo(map);
+
+/*Leyenda*/
+var legend = L.control({position: 'bottomright'});
+
+legend.onAdd = function (map) {
+
+    var div = L.DomUtil.create('div', 'info legend'),
+        grades = [0, 10, 20, 50, 100, 200, 500, 1000],
+        labels = [];
+
+    // loop through our density intervals and generate a label with a colored square for each interval
+    for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+            '<i style="background:' + getColor(grades[i] + 1) + '"></i> ' +
+            grades[i] + (grades[i + 1] ? '&ndash;' + grades[i + 1] + '<br>' : '+');
+    }
+
+    return div;
+};
+
+legend.addTo(map); 
